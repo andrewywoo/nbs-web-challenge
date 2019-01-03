4dbc1d4ccedd64b9cd694fbb31a0205cb1ee6970
Andrew Woo <andrew.woo@verizon.net>
2019-01-03 10:56:03 -0500

    modified track view bar to have spinners before track data is loaded

--- src/components/TrackViewBar/TrackViewBar.js
+++ src/components/TrackViewBar/TrackViewBar.js
@@ -1,29 +1,43 @@
 import React from "react";
+import Spinner from "../UI/Spinner/Spinner";
 import "./TrackViewBar.css";
 
 const trackViewBar = props => {
+  let style = {
+    transform: "scale(.5)",
+    margin: "0px 65px",
+    padding: "0"
+  };
   return (
     <div className="TrackViews">
       {props.trackMetrics[410] ? (
         <button onClick={props.handleTrackIdChange.bind(this, 410)}>
           {props.metricMetadata[410].fullName}
         </button>
-      ) : null}
+      ) : (
+        <Spinner />
+      )}
       {props.trackMetrics[411] ? (
         <button onClick={props.handleTrackIdChange.bind(this, 411)}>
           {props.metricMetadata[411].fullName}
         </button>
-      ) : null}
+      ) : (
+        <Spinner style={style} />
+      )}
       {props.trackMetrics[413] ? (
         <button onClick={props.handleTrackIdChange.bind(this, 413)}>
           {props.metricMetadata[413].fullName}
         </button>
-      ) : null}
+      ) : (
+        <Spinner style={style} />
+      )}
       {props.trackMetrics[414] ? (
         <button onClick={props.handleTrackIdChange.bind(this, 414)}>
           {props.metricMetadata[414].fullName}
         </button>
-      ) : null}
+      ) : (
+        <Spinner style={style} />
+      )}
     </div>
   );
 };
--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Spinner.css";
 
-const spinner = () => {
-  return <div className="Loader">Loading...</div>;
+const spinner = props => {
+  return (
+    <div style={props.style} className="Loader">
+      Loading...
+    </div>
+  );
 };
 
 export default spinner;
